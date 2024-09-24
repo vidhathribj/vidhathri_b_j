a235b08f9ed5f8c7ae6acfffcb1e177bed57349a
Vidhathri B J <161947895+vidhathribj@users.noreply.github.com>
2024-09-24 22:37:27 +0530

    Add files via upload

---
+++ _3bHW.py
@@ -0,0 +1,23 @@
+#1
+name=input("Enter name: ")
+age=input("Enter age: ")
+print(f"Hello,{name}!You are {age} years old")
+
+#2
+
+string=input("Enter sentence: ")
+print(string.upper())
+print(string.lower())
+
+print(string.replace(" ","_"))
+print(string.strip())
+
+#3
+string=input("Enter string: ")
+
+
+#4
+string="Hello\n\tWorld\nThis is a backslash:\t '\'"
+print(string)
+
+
---
+++ _4.py
@@ -0,0 +1,37 @@
+#Operators
+#Assignment operators
+x=input("Enter number: ")
+x=int(x)
+x+=2
+print(int(x))
+x-=4
+print(int(x))
+x*=6
+print(int(x))
+x/=2
+print(int(x))
+
+#Comparision operators
+x=int(input("Enter value of x: "))
+y=int(input("Enter value of y: "))
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+
+#Logical operator
+a=2
+b=4
+
+print(a>4 and b<4)
+print(a>0 and b<4)
+print(a>0 and b<54)
+
+print(a>4 or b<4)
+print(a>0 or b<4)
+print(a>0 or b<5)
+
+
+
+
