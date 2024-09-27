e98fa86a1bc4ce52974de95fcf7adca7185cef91
Vidhathri B J <161947895+vidhathribj@users.noreply.github.com>
2024-09-27 17:30:17 +0530

    Add files via upload

--- _5HW.py
+++ _5HW.py
@@ -21,10 +21,30 @@ Prints the entire matrix row by row.
 Prints the sum of each row in the matrix.'''
 
 list1=[]
+
 n=int(input("Enter the number of elements in the list: "))
+m=int(input("Enter the size of inner list: "))
 print("Enter elements:")
 for i in range (0,n):
-   ele=int(input())
-   list1.append(ele)    
+   list2=[]
+   for j in range(0,m):
+     ele=int(input())
+     list2.append(ele)
+   print(sum(list2))
+   list1.append(list2)
+ 
 print(list1)
+
+#3Reverse and Sort a List: Create a list of numbers and:
+'''Sort it in descending order.
+Reverse the sorted list and print it.'''
+
+list3=[1,4,3,82,35,24]
+list3.sort(reverse=True)
+print(list3)
+
+list3.reverse()
+print(list3)
+
+  
 
